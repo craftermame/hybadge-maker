4acb9656c95e5d5ac9fc8c854517b3f800d090c2
K.Owada <kaiji.owada.256@gmail.com>
2026-02-19 04:29:42 +0900

    feat: build bin with assets

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source .venv/bin/activate
-python -m PyInstaller --onefile --paths src src/app/main.py
+python -m PyInstaller --onefile --paths src --add-data "assets/fonts:assets/fonts" src/app/main.py
 
 cp dist/main ui/bin/
